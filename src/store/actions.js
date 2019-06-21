704e3a1c228c774671851fea2db5554d48113265
Carlos Sandoval <iznomag@gmail.com>
2019-06-21 15:54:03 -0500

    Feature redux (#9)
    * Add home view
    
    * Resolve rebase
    
    * Add Categorize View

---
+++ src/store/actions.js
@@ -0,0 +1,18 @@
+function Add(data) {
+  const id = Date.now();
+  const date = new Date();
+  const month = date.toLocaleDateString("en-us", { month: "long" });
+  console.log(data);
+  return {
+    type: "ADD",
+    payload: {
+      id,
+      type: data.type,
+      category: data.category,
+      amount: data.amount,
+      month
+    }
+  };
+}
+
+export { Add };
