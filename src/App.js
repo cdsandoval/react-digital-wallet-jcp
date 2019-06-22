93d477889a739534712984a3908f5c2e6aa2acf6
Carlos Sandoval <iznomag@gmail.com>
2019-06-22 13:49:25 -0500

    Adding CSS HOME

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+/** @jsx jsx */
 import React from "react";
 import Report from "./views/monthly-report";
+import { jsx } from "@emotion/core";
 import Categorize from "./views/categorize";
 import { Router } from "@reach/router";
 import Home from "./views/home";
@@ -7,7 +9,15 @@ import AddComponent from "./components/add";
 
 function App() {
   return (
-    <div>
+    <div
+      css={{
+        width: "800px",
+        margin: "0 auto",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
       <Router>
         <Report path="/report" />
         <Home path="/" />
--- src/views/home.js
+++ src/views/home.js
@@ -2,20 +2,44 @@
 import React from "react";
 import { jsx } from "@emotion/core";
 import { connect } from "react-redux";
+import { FaUser } from "react-icons/fa";
 
 import Button from "../components/button";
 
 function Home(props) {
   return (
     <>
-      <div>
-        <Button path="/add">Add</Button>
-        <Button path="/report">Report</Button>
-        {/* TODO: Refactor report link */}
-        <Button path="/report/idMonth">Report of the Month</Button>
+      <div
+        css={{
+          display: "flex",
+          justifyContent: "space-between",
+          backgroundColor: "#eee",
+          padding: "10px",
+          width: "300px"
+        }}
+      >
+        <h2
+          css={{
+            fontFamily: "Arial, Helvetica, sans-serif",
+            alignSelf: "center"
+          }}
+        >
+          My wallet
+        </h2>
+        <img
+          css={{
+            alignSelf: "center",
+            width: "40px",
+            height: "40px",
+            borderRadius: "50%",
+            backgroundColor: "black"
+          }}
+          src="https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1"
+        />
       </div>
+
       <div>
-        <table>
+        <table css={{ textAlign: "center", margin: "30px auto" }}>
           <tr>
             <th>Category</th>
             <th>Amount</th>
@@ -31,6 +55,11 @@ function Home(props) {
             );
           })}
         </table>
+      </div>
+      <div>
+        <Button path="/add">Add</Button>
+        <Button path="/report">Report</Button>
+        <Button path="/report/idMonth">Report of the Month</Button>
       </div>
     </>
   );
