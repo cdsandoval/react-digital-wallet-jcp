d8b0d4ebebebfd3d5c56984c466987f09299df14
Carlos Sandoval <iznomag@gmail.com>
2019-06-21 20:42:23 -0500

    Add monthly report with redux

--- src/views/monthly-report.jsx
+++ src/views/monthly-report.jsx
@@ -4,13 +4,42 @@ import { jsx, css } from "@emotion/core";
 import { container, header } from "../utils/monthly-css";
 import { connect } from "react-redux";
 
-function MonthlyReport() {
+function MonthlyReport(props) {
+  const [deposit, setDeposit] = React.useState(0);
+  const [withdraw, setWithdraw] = React.useState(0);
+  const [total, setTotal] = React.useState(0);
+
+  React.useEffect(() => {
+    setDeposit(filter(1, getMonth()));
+    setWithdraw(filter(0, getMonth()));
+    setTotal(saldo());
+  }, [withdraw]);
+
+  function filter(type, month) {
+    let result = props.wallet
+      .filter(value => value.mes === month && value.type === type)
+      .reduce((total, value) => total + value.amount, 0);
+    return result;
+  }
+
+  function saldo() {
+    let saldo = props.total.map(
+      value => value.month == getMonth() && value.total
+    );
+    return saldo.toString();
+  }
+
+  function getMonth() {
+    const date = new Date();
+    return date.toLocaleDateString("en-us", { month: "long" });
+  }
+
   return (
     <div css={container}>
-      <p css={header}>Saldo: 3000</p>
+      <p css={header}>Saldo: {total}</p>
       <div>
-        <p>Ingreso: 1000</p>
-        <p>Gastos: 500</p>
+        <p>Deposit: {deposit}</p>
+        <p>Withdraw: {withdraw}</p>
       </div>
     </div>
   );
@@ -18,7 +47,8 @@ function MonthlyReport() {
 
 function mapState(state) {
   return {
-    wallet: state.wallet
+    wallet: state.wallet,
+    total: state.totalMonthly
   };
 }
 
