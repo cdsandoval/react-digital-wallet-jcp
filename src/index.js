980af56fa55085172cc392683f6b875286a2fa96
Carlos Sandoval <iznomag@gmail.com>
2019-06-24 16:27:41 -0500

    Add PWA

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { register } from "./service-worker";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -10,3 +11,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+register();
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,9 +1,45 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import reducer from "./reducer";
 import logger from "redux-logger";
+import offlineSync from "../middlewares/offline-sync";
 
 const composer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composer(applyMiddleware(logger)));
+if (localStorage.getItem("version") !== process.env.REACT_APP_VERSION) {
+  localStorage.removeItem("state");
+}
+
+const initialState = {
+  type: {
+    withdraw: ["Transporte", "Comida", "Servicios basicos", "Extras"],
+    deposit: ["Sueldo Fijo", "Extras"],
+    month: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ]
+  },
+  wallet: {},
+  totalMonthly: {}
+};
+
+const preloadedState = JSON.parse(
+  localStorage.getItem("state") || JSON.stringify(initialState)
+);
+
+const store = createStore(
+  reducer,
+  preloadedState,
+  composer(applyMiddleware(offlineSync, logger))
+);
 
 export default store;
